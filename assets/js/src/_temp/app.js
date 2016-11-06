deb35977fc3815fe5820542eae2049819358e422
bgsouza <bruno@bgsouza.com>
2016-11-06 13:25:51 -0200

    js

---
+++ assets/js/src/_temp/app.js
@@ -0,0 +1,28 @@
+var Slide = function($container) {
+  this.$carrossel = typeof $container != 'undefined' ? $container : '.carrossel-ul';
+
+  this.init = function() {
+    this.galeria();      
+  }
+
+  this.galeria = function() {
+      var carousel = new ch.Carousel(ch(this.$carrossel)[0], {
+          pagination: true
+      });
+  }
+}
+
+;(function() {
+  var $slide = new Slide();
+  $slide.init();
+})();
+
+var MainMeli = {
+  init: function() {
+
+  },
+}
+
+;(function() {
+  MainMeli.init();
+})
